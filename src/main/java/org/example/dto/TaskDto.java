ceb65d16d03e2f98bf4c6bd933ffcc9926a206c6
Ilya <ism-ektb@yandex.ru>
2024-11-30 11:34:10 +0500

    deploy

--- src/main/java/org/example/dto/TaskDto.java
+++ src/main/java/org/example/dto/TaskDto.java
@@ -2,9 +2,11 @@ package org.example.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 @Data
+@EqualsAndHashCode(of ={"id"})
 public class TaskDto {
 
     private Long id;
--- src/test/java/org/example/TaskServiceImplTest.java
+++ src/test/java/org/example/TaskServiceImplTest.java
@@ -1,50 +1,46 @@
 package org.example;
 
-import lombok.RequiredArgsConstructor;
 import org.example.dto.NewTaskDto;
 import org.example.dto.TaskDto;
 import org.example.dto.UpdateTaskDto;
-import org.example.exeptions.ConflictException;
 import org.example.exeptions.StorageException;
+import org.example.mapper.TaskMapper;
 import org.example.model.Task;
-import org.example.repository.TaskRepository;
 import org.example.service.TaskServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.logging.Filter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TaskServiceImplTest {
-@Autowired
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:15");
+
+    @Autowired
     private TaskServiceImpl taskService;
-
-    @MockBean
-    private TaskRepository taskRepository;
-
-
     private Task task;
+    @Autowired
+    private TaskMapper mapper;
     private TaskDto taskDto;
     private LocalDateTime now = LocalDateTime.now();
 
@@ -75,6 +71,7 @@ public class TaskServiceImplTest {
     }
 
     @Test
+    @Order(1)
     void testCreate() {
         NewTaskDto newTask = new NewTaskDto();
         newTask.setTitle(task.getTitle());
@@ -83,7 +80,6 @@ public class TaskServiceImplTest {
         newTask.setAssigneeId(task.getAssigneeId());
         newTask.setAuthorId(task.getAuthorId());
         newTask.setEventId(task.getEventId());
-        when(taskRepository.save(any(Task.class))).thenReturn(task);
         TaskDto result = taskService.createTask(newTask);
 
         assertEquals(taskDto, result);
@@ -93,6 +89,7 @@ public class TaskServiceImplTest {
     }
 
     @Test
+    @Order(2)
     void testUpdate() {
         UpdateTaskDto updateTask = new UpdateTaskDto();
         updateTask.setTitle(task.getTitle());
@@ -122,19 +119,12 @@ public class TaskServiceImplTest {
         newTaskDto.setAssigneeId(newTask.getAssigneeId());
         newTaskDto.setEventId(newTask.getEventId());
         newTaskDto.setAuthorId(newTask.getAuthorId());
-        when(taskRepository.findById(any(Long.class))).thenReturn(Optional.of(task));
-        when(taskRepository.save(any(Task.class))).thenReturn(newTask);
-
-        TaskDto result = taskService.updateTask(1L,1L, updateTask);
-
-        assertNotEquals(taskDto, result);
-        assertEquals(task.getId(), result.getId());
-        assertEquals(task.getDescription(), result.getDescription());
-        assertEquals(task.getDeadline(), result.getDeadline());
-        assertEquals(task.getAuthorId(), result.getAuthorId());
+        TaskDto result = taskService.updateTask(1L, 1L, updateTask);
+        assertEquals(mapper.modelToDto(task), result);
     }
 
     @Test
+    @Order(3)
     void testUpdateWrongId(){
         UpdateTaskDto updateTask = new UpdateTaskDto();
         updateTask.setTitle(task.getTitle());
@@ -148,6 +138,7 @@ public class TaskServiceImplTest {
     }
 
     @Test
+    @Order(4)
     void getTasksTest(){
         List<TaskDto> listDto = new ArrayList<>();
         listDto.add(taskDto);
@@ -155,9 +146,7 @@ public class TaskServiceImplTest {
         list.add(task);
         Page<Task> tasks = new PageImpl<>(list);
         Pageable page = PageRequest.of(0, 10);
-        when(taskRepository.findByFilters(anyLong(),anyLong(),anyLong(),any(Pageable.class))).thenReturn(tasks);
-
-        List<TaskDto> result = taskService.getTasks(0,1,1L,2L,1L);
+        List<TaskDto> result = taskService.getTasks(0, 1, 1L, 1L, 2L);
 
         assertEquals(1, result.size());
         assertEquals(task.getId(), result.get(0).getId());
